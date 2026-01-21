00aed37a1e3c9fc39964fd728998fdf42887af9b
arpitash13@gmail.com <sharmaarpita2005@gmail.com>
2026-01-21 13:59:14 +0530

    api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://healthlink-app-1.onrender.com";
 
 export const submitSupportRequest = async (data) => {
   const response = await fetch(`${API_BASE_URL}/support`, {
